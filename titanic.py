d466b149577083de14e1cbc844dd22a022a2b600
Rohit Rawat <xordux@gmail.com>
2016-06-06 05:07:04 +0530

    use Random forest instead of decision trees

--- titanic.py
+++ titanic.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 
 train_url = "trainingData/train.csv"
@@ -23,27 +23,18 @@ train["Embarked"][train["Embarked"] == "Q"] = 2
 train["Age"] = train["Age"].fillna(train["Age"].median())
 
 # Adding a feature to improve prediction:
-train["family_size"] = train["SibSp"].values + train["Parch"].values + 1
+# train["family_size"] = train["SibSp"].values + train["Parch"].values + 1
 
-#cross_val = train[800:]
-#train = train[1:800]
-
-feature_names = ["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "family_size"]
-
-# to control overfitting: 
-max_depth = 10
-min_samples_split = 5
+feature_names = ["Pclass", "Sex", "Age", "Fare", "SibSp", "Parch", "Embarked"]
 
 target = train["Survived"].values
 features_one = train[feature_names].values
 
 #Fitting the selected features in a decision tree
-my_tree_one = tree.DecisionTreeClassifier(max_depth = max_depth, min_samples_split = min_samples_split, random_state = 1)
-my_tree_one = my_tree_one.fit(features_one, target)
-
-#cross_target = cross_val["Survived"]
-#cross_features = cross_val[feature_names].values
-#print(my_tree_one.score(cross_features , cross_target))
+max_depth = 10
+min_samples_split = 2
+my_forest = RandomForestClassifier(max_depth = max_depth, min_samples_split = min_samples_split, n_estimators = 100, random_state = 1)
+my_forest = my_forest.fit(features_one, target)
 
 # -------------------------------------------------------------------------------
 # ------------------------------- Test cases ------------------------------------
@@ -70,7 +61,7 @@ test["family_size"] = test["SibSp"].values + test["Parch"].values + 1
 test_features = test[feature_names].values
 
 # Make your prediction using the test set
-my_prediction = my_tree_one.predict(test_features)
+my_prediction = my_forest.predict(test_features)
 
 PassengerId = np.array(test["PassengerId"]).astype(int)
 my_solution = pd.DataFrame(my_prediction, PassengerId, columns = ["Survived"])
